47e404b97153b49b51c5ec0fd864f954360f0375
Mayank Choudhary <mayank.choudhary@openenglish.com>
2024-04-28 15:42:39 +0530

    added roles and responsibilities

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -1,7 +1,9 @@
 // @flow strict
+"use client";
 
 import { personalData } from "@/utils/data/personal-data";
 import Image from "next/image";
+import { ReactTyped } from "react-typed";
 
 
 function AboutSection() {
@@ -16,11 +18,20 @@ function AboutSection() {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
         <div className="order-2 lg:order-1">
           <p className="font-medium mb-5 text-[#16f2b3] text-xl uppercase">
-            Who I am?
+            Roles & Responsibilities
           </p>
-          <p className="text-gray-200 text-sm lg:text-lg">
-            {personalData.description}
-          </p>
+          <div className="text-gray-200 text-sm lg:text-lg scrollbox">
+            <ReactTyped
+              strings={[personalData.roles]}
+              typeSpeed={50}
+              loop={false}
+              backSpeed={20}
+              cursorChar=">"
+              showCursor={true}
+            />
+
+          </div>
+
         </div>
         <div className="flex justify-center order-1 lg:order-2">
           <Image
