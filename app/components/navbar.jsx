29629e80bfde5f4aeb61d393580b0b36e9aff39d
Mayank Choudhary <mayank.choudhary@openenglish.com>
2024-05-02 00:44:55 +0530

    HIGHLIGHTS Added

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,17 +1,29 @@
 // @flow strict
+
+"use client";
+import React from 'react';
 import Link from "next/link";
+import TextTransition, { presets } from 'react-text-transition';
+import { highlights } from "@/utils/data/highlights";
+
 
 
 function Navbar() {
+  const [index, setIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      3000, // every 3 seconds
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
+
   return (
     <nav className="bg-transparent">
       <div className="flex items-center justify-between py-5">
         <div className="flex flex-shrink-0 items-center">
-          <Link
-            href="/"
-            className=" text-[#16f2b3] text-3xl font-bold">
-           MAYANK KUMAR
-          </Link>
+          <TextTransition  className=" text-[#16f2b3] text-3xl font-bold" springConfig={presets.wobbly}>{highlights[index % highlights.length]}</TextTransition>
         </div>
 
         <ul className="mt-4 flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100" id="navbar-default">
