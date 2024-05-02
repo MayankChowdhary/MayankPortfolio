4026f4a1e992314bf577870d00e22bab5468e641
Mayank Choudhary <mayank.choudhary@openenglish.com>
2024-05-02 21:30:03 +0530

    added projects data

--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -59,7 +59,8 @@ function ProjectCard({ project }) {
           <div className="ml-4 lg:ml-8 mr-2">
             <span className="text-white">Description:</span>
             <span className="text-cyan-400">{' ' + project.description}</span>
-            <span className="text-gray-400">,</span>
+            <span className="text-gray-400">,</span> <br/><br/>
+            <a className={project.demo===""? 'hidden' : 'project-links'} href={project.demo}>▶️ View this app on Play Store</a>
           </div>
           <div><span className="text-gray-400">{`};`}</span></div>
         </code>
