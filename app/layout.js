17cb539b3a14e59d1f79e0f857ee37acfdfeaf7a
Mayank Choudhary <mayank.choudhary@openenglish.com>
2024-05-09 23:35:51 +0530

    import pattern changed

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from './components/footer';
 import Navbar from './components/navbar';
-import './css/card.scss';
 import './css/globals.scss';
+import './css/card.scss';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    domains: ['res.cloudinary.com', 'media.dev.to', 'picsum.photos', 'raw.githubusercontent.com']
+    domains: ['res.cloudinary.com', 'media.dev.to', 'picsum.photos', 'raw.githubusercontent.com','drive.google.com','accounts.google.com']
   },
   webpack: (config) => {
       config.resolve.alias.canvas = false;
