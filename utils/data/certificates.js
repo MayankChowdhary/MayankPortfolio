349e47bf07d6f2ac4a921232da101acd9fa77d61
Mayank Choudhary <mayank.choudhary@openenglish.com>
2024-05-12 21:48:33 +0530

    numbers added in certifications

--- utils/data/certificates.js
+++ utils/data/certificates.js
@@ -1,37 +1,37 @@
 export const certificates = [
   {
     id: 1,
-    title: "Apache Maven",
+    title: "1. Apache Maven",
     file: "https://drive.google.com/file/d/1IcCtpURBKiAqTpM5UerEJbnCzwMoyzGP/preview",
     institution: "SimpliLearn",
   },
   {
     id: 2,
-    title: "Core Java",
+    title: "2. Core Java",
     file: "https://drive.google.com/file/d/1e5ORy0ukfWhfRxHgI2llUqPCMaksc8eM/preview",
     institution: "SimpliLearn",
   },
   {
     id: 3,
-    title: "Docker Compose In Depth",
+    title: "3. Docker Compose In Depth",
     file: "https://drive.google.com/file/d/19jWYaArzDwAjJD38jF6uq-wXCAbquOMO/preview",
     institution: "SimpliLearn",
   },
   {
     id: 4,
-    title: "Fundamentals of JavaScript",
+    title: "4. Fundamentals of JavaScript",
     file: "https://drive.google.com/file/d/1McemVjlnxPlMnuyF0mB25jGWsK1opZDI/preview",
     institution: "SimpliLearn",
   },
   {
     id: 5,
-    title: "All in One Python Development Suite",
+    title: "5. All in One Python Development Suite",
     file: "https://drive.google.com/file/d/1TaMrqfLWLf7vNESu8lGqzDOsJCNBiIn4/preview",
     institution: "SimpliLearn",
   },
   {
     id: 6,
-    title: "Selenium WebDriver with Python",
+    title: "6. Selenium WebDriver with Python",
     file: "https://drive.google.com/file/d/1jrn0PWxkNWE-W0FI1lAqkun3QRU9-MBC/preview",
     institution: "Udemy",
   }
